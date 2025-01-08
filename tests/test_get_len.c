bc8eb7e00481d7c173a1c647ecb921fe44a2959f
Ilia Munaev <ilyamunaev@gmail.com>
2025-01-08 18:24:30 +0200

    Feat: add more test cases to get_len()

--- tests/test_get_len.c
+++ tests/test_get_len.c
@@ -6,7 +6,7 @@
 /*   By: imunaev- <imunaev-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 18:16:52 by imunaev-          #+#    #+#             */
-/*   Updated: 2025/01/08 18:16:53 by imunaev-         ###   ########.fr       */
+/*   Updated: 2025/01/08 18:23:52 by imunaev-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,23 @@
 
 void test_get_len()
 {
-	char *av1[] = {"./prog", "hello world"};
+	char *av1[] = {"./prog", "1 2"};
 	assert(get_len(2, av1) == 2);
 
 	char *av2[] = {"./prog", "   "};
-	assert(get_len(2, av2) == 0);  // No words, 0 characters
+	assert(get_len(2, av2) == 0);
 
-	char *av3[] = {"./prog", "  a   b  ", "test case"};
-	assert(get_len(3, av3) == 4);  // "a", "b", "test", "case"
+	char *av3[] = {"./prog", "  -1   2  ", "54 42"};
+	assert(get_len(3, av3) == 4);
 
 	char *av4[] = {"./prog", ""};
-	assert(get_len(2, av4) == 0);  // Empty argument, 0 characters
+	assert(get_len(2, av4) == 0);
 
-	char *av5[] = {"./prog", "  one ", " two three "};
-	assert(get_len(3, av5) == 3);  // "one", "two", "three"
+	char *av5[] = {"./prog", "  -234 ", " 777 two	"};
+	assert(get_len(3, av5) == 3);
+
+	char *av6[] = {"./prog", "	", "	two	 1 "};
+	assert(get_len(3, av6) == 2);
 
 	printf("All tests for get_len passed.\n");
 }
