f452b11d983f7c225b311adc3d3cbf7f68eb9771
Ilia Munaev <ilyamunaev@gmail.com>
2025-01-10 10:46:49 +0200

    Feat: split init_stack into two function

--- src/init_stack.c
+++ src/init_stack.c
@@ -6,7 +6,7 @@
 /*   By: imunaev- <imunaev-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 10:49:30 by imunaev-          #+#    #+#             */
-/*   Updated: 2025/01/10 10:31:18 by imunaev-         ###   ########.fr       */
+/*   Updated: 2025/01/10 10:44:32 by imunaev-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -177,7 +177,7 @@ char	***split_arrs(int ac, char **av)
 	arrs[j] = NULL;
 	return (arrs);
 }
-
+/*
 t_stacks	*init_stack(int ac, char **av)
 {
 	int	*arr;
@@ -201,6 +201,55 @@ t_stacks	*init_stack(int ac, char **av)
 		error_exit(stx);
 	stx->b->head = NULL;
 	stx->b->size = 0;
+
+	arrs = split_arrs(ac, av);
+	if (!arrs)
+		error_exit(stx);
+
+	size = get_len_arr(arrs);
+	if(size == 0)
+		program_exit(stx, arrs);
+
+	arr = get_int_arr(size, arrs);
+	if (!arr)
+	{
+		free_split_arrs(arrs);
+		return (NULL);
+	}
+	stx = fill_up_stack(stx, arr, size);
+	free(arr);
+	return (stx);
+}
+*/
+t_stacks	*creat_stacks(void)
+{
+	t_stacks	*stx;
+	stx = malloc(sizeof(t_stacks));
+		if (!stx)
+			return (NULL);
+	stx->a = NULL;
+	stx->b = NULL;
+	stx->a = malloc(sizeof(t_stack));
+	if (!stx->a)
+		error_exit(stx);
+	stx->a->head = NULL;
+	stx->a->size = 0;
+	stx->b = malloc(sizeof(t_stack));
+	if (!stx->b)
+		error_exit(stx);
+	stx->b->head = NULL;
+	stx->b->size = 0;
+	return(stx);
+}
+
+t_stacks	*init_stack(int ac, char **av)
+{
+	int	*arr;
+	int	size;
+	t_stacks	*stx;
+	char 	***arrs;
+
+	stx = creat_stacks();
 
 	arrs = split_arrs(ac, av);
 	if (!arrs)
